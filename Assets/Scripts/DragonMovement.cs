898c9f9057938275f642bf5da94682e0877ceb1a
Sonia O <sonicu.olar@gmail.com>
2025-09-16 18:29:46 -0400

    Made folders and created DragonTesting Scene

---
+++ Assets/Scripts/DragonMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class dragon : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 pos = transform.position;
+
+        pos.x += h * speed * Time.deltaTime;
+        pos.y += v * speed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
