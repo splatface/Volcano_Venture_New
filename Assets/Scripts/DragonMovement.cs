03f4a24f2b13505ce9b0617809a821bedd15f2c3
Sonia O <sonicu.olar@gmail.com>
2025-09-18 20:05:22 -0400

    Gave dragon walking animation and gave it jump physics.

--- Assets/Scripts/DragonMovement.cs
+++ Assets/Scripts/DragonMovement.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 public class dragon : MonoBehaviour
 {
     [SerializeField]
-    public float moveForce = 10f;
+    public float moveForce = 2f;
 
     [SerializeField]
     public float jumpForce = 11f;
 
     private float movementX;
-
     private Rigidbody2D dragonBody;
     private SpriteRenderer sr;
     private Animator anim;
@@ -20,20 +19,55 @@ public class dragon : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        dragonBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        Vector2 pos = transform.position;
-
-        pos.x += h * speed * Time.deltaTime;
-        pos.y += v * speed * Time.deltaTime;
-
-        transform.position = pos;
+        DragonMoveKeyboard();
+        AnimateDragon();
+        DragonJump();
     }
+
+    private void FixedUpdate()
+    {
+        DragonJump();
+    }
+
+    void DragonMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimateDragon()
+    {
+        if (movementX > 0)
+        {
+            sr.flipX = true;
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+        else if (movementX < 0)
+        {
+            sr.flipX = false;
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+    void DragonJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            dragonBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
 }
